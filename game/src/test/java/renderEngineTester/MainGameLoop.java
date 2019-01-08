fb2e45ae16d0855f7ee94d8388599a793694c9c0
MichalKleinGitHub <42419041+MichalKleinGitHub@users.noreply.github.com>
2019-01-08 16:34:36 +0100

    Add files via upload

--- game/src/test/java/renderEngineTester/MainGameLoop.java
+++ game/src/test/java/renderEngineTester/MainGameLoop.java
@@ -1,11 +1,13 @@
 package renderEngineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
-import renderEngine.shaders.StaticShader;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 /**
  * MainGameLoop is the public class for testing renderEngine components.
@@ -47,7 +49,7 @@ public class MainGameLoop {
                 -0.5f, 0.5f, 0,    //V0
                 -0.5f, -0.5f, 0,   //V1
                 0.5f, -0.5f, 0,    //V2
-                0.5f, 0.5f, 0    //V3
+                0.5f, 0.5f, 0      //V3
         };
 
         int[] indices = {
@@ -55,13 +57,22 @@ public class MainGameLoop {
                 3,1,2   //Bottom right triangle (V3,V1,V2)
         };
 
-        RawModel model = loader.loadToVAO(vertices,indices);
+        float[] textureCoords = {
+                0,0,    //V0
+                0,1,    //V1
+                1,1,    //V2
+                1,0     //V3
+        };
+
+        RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
 
         //The main game loop
         while(!Display.isCloseRequested()){
             renderer.prepare();
             shader.start();
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
             DisplayManager.updateDisplay();
         }
